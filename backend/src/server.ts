6df4f9f2cc4f7d06db2e2f6c1dc2143f16bcd2cd
Sunny Mishra <sunnycoding712740@gmail.com>
2026-04-29 20:30:34 +0530

    Server is Running

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
